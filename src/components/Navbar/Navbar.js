f77b6eb21bebda334fccf7048f2ce3a610aea9e0
felipefafm <felipealbertofloresm@gmail.com>
2022-11-27 20:45:14 -0300

    componentes II
    boton sumar restar y reset para el ItemCount

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,58 +1,4 @@
-// const Navbar=() =>{
-//     return(
 
-//         <nav className='navbar'>
-//             <h1>Hola mundo</h1>
-
-//         </nav>
-
-//     )
-// }
-
-// export default Navbar
-// import Nav from 'react-bootstrap/Nav';
-
-// function Navbar() {
-//   return (
-//     <>
-//       <Nav className="justify-content-center" activeKey="/home">
-//         <Nav.Item>
-//           <Nav.Link href="/home">Active</Nav.Link>
-//         </Nav.Item>
-//         <Nav.Item>
-//           <Nav.Link eventKey="link-1">Link</Nav.Link>
-//         </Nav.Item>
-//         <Nav.Item>
-//           <Nav.Link eventKey="link-2">Link</Nav.Link>
-//         </Nav.Item>
-//         <Nav.Item>
-//           <Nav.Link eventKey="disabled" disabled>
-//             Disabled
-//           </Nav.Link>
-//         </Nav.Item>
-//       </Nav>
-//       <p className="text-center mt-4 mb-4">Or right-aligned</p>
-//       <Nav className="justify-content-end" activeKey="/home">
-//         <Nav.Item>
-//           <Nav.Link href="/home">Active</Nav.Link>
-//         </Nav.Item>
-//         <Nav.Item>
-//           <Nav.Link eventKey="link-1">Link</Nav.Link>
-//         </Nav.Item>
-//         <Nav.Item>
-//           <Nav.Link eventKey="link-2">Link</Nav.Link>
-//         </Nav.Item>
-//         <Nav.Item>
-//           <Nav.Link eventKey="disabled" disabled>
-//             Disabled
-//           </Nav.Link>
-//         </Nav.Item>
-//       </Nav>
-//     </>
-//   );
-// }
-
-// export default Navbar;
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -61,7 +7,6 @@ import CartWidget from '../CartWidget/CartWidget';
 import Alert from 'react-bootstrap/Alert'
 import BotonNav from '../BotonNav/BotonNav';
 
-// import logovc from '*.src/components/logovc.png'
 
 function CollapsNav(props) {
 
@@ -83,19 +28,7 @@ function CollapsNav(props) {
           <BotonNav textoNav="Cap" func={handleOnClick} />
           <BotonNav textoNav="Gorras" />
           <BotonNav textoNav="Contacto" />
-            {/* <Nav.Link href="#cap" func={handleOnClick}>Cap</Nav.Link>
-            <Nav.Link href="#Gorras" func={onClick2}>Gorras</Nav.Link> */}
-            {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown> */}
+           
           </Nav>
           <Nav>
 
